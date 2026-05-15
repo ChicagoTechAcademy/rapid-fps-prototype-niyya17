e44a71427366200b64b1552d70e69855fbea4263
niyya17 <japassley1@chicagotechacademy.org>
2026-05-15 11:48:05 -0500

    complete some of video 6

---
+++ Source/RapidFPS/Target.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Target.generated.h"
+
+class USphereComponent;
+class UStaticMeshComponet;
+
+UCLASS()
+class RAPIDFPS_API ATarget : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATarget();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere)
+	USphereComponent* Collision;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UStaticMeshComponent* Mesh;
+
+};
